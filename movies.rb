cd666c44f2e041beffc0efca50beccdda7fbb57b
nikfor <nikfor1@gmail.com>
2015-10-24 20:56:45 +0300

    second-task first attempt

--- movies.rb
+++ movies.rb
@@ -1,3 +1,4 @@
+=begin
 good_movies =  [ 'Matrix', '300 Spartans', 'Dejavu', 'Spy games', 'Lucky number Slevin' ]
 bad_movies = %w{ Titanic Hatico Witch }
 
@@ -10,5 +11,14 @@ elsif bad_movies.include?(all_str)
 else
   puts "Haven't seen #{all_str} yet"
 end
-  
+=end
+if File.exist?(ARGV[0])
+  fields = File.readlines( "movies.txt" )
+  fields.map! { |e| e.chomp }
+  fields.map! { |e|  e.split("|") }
+  fields.each{ |i| puts i[1] + " " + "*" * i[7][2].to_i if i[1].include? "Time " }
+else
+  puts "File not found:" + ARGV[0]
+end
+
 
