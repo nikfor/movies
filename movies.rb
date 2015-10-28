02b7db15c1c6ff15dfe5bda56ce9c98df9884669
nikfor <nikfor1@gmail.com>
2015-10-28 19:36:45 +0300

    third-task sec attempt

--- movies.rb
+++ movies.rb
@@ -11,7 +11,7 @@ elsif bad_movies.include?(all_str)
 else
   puts "Haven't seen #{all_str} yet"
 end
-=end
+
 unless File.exist?(ARGV[0])
   puts "File not found:" + ARGV[0]
   exit 0
@@ -20,43 +20,42 @@ fields = File.readlines( "movies.txt" )
 fields.map! { |e| e.chomp }
 fields.map! { |e|  e.split("|") }
 fields.each{ |i| puts i[1] + " " + "*" * i[7][2].to_i if i[1].include? "Time " }
+=end
 
-# 3.1
-fields.map! do |i|              
-  { "url" => i[0], "name" => i[1], "year" => i[2], "country" => i[3], "date" => i[4], "genre" => i[5], "duration" => i[6], "point" => i[7], "author" => i[8], "actors" => i[9] }
-end
+films = File.readlines( "movies.txt" ).
+  map { |ln| ln.chomp }.
+  map { |ln|  ln.split("|") }.
+  map { |f| { url: f[0], name: f[1], year: f[2], country: f[3], date: f[4], genre: f[5], duration: f[6], point: f[7], author: f[8], actors: f[9] }}
+
 
 # 3.2
-sort = fields.sort_by do |i|    
-  i["duration"].to_i
-end
-puts sort.reverse![0..4]
+
+puts films.sort_by { |f| f[:duration].to_i }.last(5)
 
 
-sort = fields.sort_by do |i|    # 3.3
-  i["date"]
+# 3.3
+
+sort_films = films.sort_by do |f|    
+  f[:date]
 end
-sort.delete_if do |i|
-  !i["genre"].include? "Comedy"
+sort_films.delete_if do |f|
+  !f[:genre].include? "Comedy"
 end
-puts sort
+puts sort_films
+ 
 
 # 3.4
-sort = fields.sort_by do |i|    
-  i["author"]
-end
-a = []
-sort.each {|i| a.push(i["author"])}
 
-puts a.uniq!
-                           
-
+films.sort_by { |f| f[:author]}.
+  map { |ln| ln[:author] }.uniq!.each{ |f| puts f}
+        
+                      
 # 3.5
 
-fields.delete_if do |i|
-  i["country"].include? "USA"
+films.delete_if do |f|
+  f[:country].include? "USA"
 end
 
-puts fields.count
+puts films.count
 
 
