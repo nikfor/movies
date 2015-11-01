3ee70e35a3b66d4ec38786cbdebf950a61438d31
nikfor <nikfor1@gmail.com>
2015-11-01 14:56:46 +0300

    third-task fourth attempt

--- movies.rb
+++ movies.rb
@@ -21,41 +21,55 @@ fields.map! { |e| e.chomp }
 fields.map! { |e|  e.split("|") }
 fields.each{ |i| puts i[1] + " " + "*" * i[7][2].to_i if i[1].include? "Time " }
 =end
+name_fields = [:url, :name, :year, :country, :date, :genre, :duration, :point, :author, :actors]
 
 films = File.readlines( "movies.txt" ).
   map { |ln| ln.chomp }.
-  map { |ln|  ln.split("|") }.
-  map { |f| { url: f[0], name: f[1], year: f[2], country: f[3], date: f[4], genre: f[5], duration: f[6], point: f[7], author: f[8], actors: f[9] }}
+  map { |ln|  Hash[*ln.split("|").zip(name_fields).flatten].invert }
 
 
 # 3.2
-
-puts films.sort_by { |f| f[:duration].to_i }.last(5)
+puts "------------------------------------------------------------\nFive longest films:"
+films.sort_by { |f| f[:duration].to_i }.last(5).reverse.each{|f| puts f[:name]+' - '+f[:genre]+' - '+f[:duration] }
 
 
 # 3.3
+puts "------------------------------------------------------------\nComedy films:"
+films.sort_by { |f| f[:date] }.select{ |f| f[:genre].include? "Comedy"}.each{|f| puts f[:name]+' - '+f[:genre]+' - '+f[:duration] }
 
-sort_films = films.sort_by do |f|    
-  f[:date]
-end
-sort_films.delete_if do |f|
-  !f[:genre].include? "Comedy"
-end
-puts sort_films
  
 
 # 3.4
-
+puts "------------------------------------------------------------\nAll directors alphabetically:"
 films.sort_by { |f| f[:author]}.
   map { |ln| ln[:author] }.uniq!.each{ |f| puts f}
         
                       
 # 3.5
+puts "------------------------------------------------------------\nCount films shot not in the USA:"
+puts films.reject { |f| f[:country].include? "USA"}.count
 
-films.delete_if do |f|
-  f[:country].include? "USA"
+# bonus 1
+puts "------------------------------------------------------------\nGroup films by produce:"
+films.group_by{ |f| f[:author]}.each{|f| puts f}
+=end
+
+# bonus 2
+
+puts "------------------------------------------------------------\nHow many time was removed each actor:"
+actors_arr = films.map{ |f| f[:actors].split(",")}.flatten.uniq!.sort #each{ |a| puts a + " - " + films.count(a){|f, a| f[:actors].include? a }.to_s }
+i = 0
+while  i < actors_arr.size do     #не в стиле руби(( но ничего не получилось
+  count_actor = 0
+  j = 0
+  while j < films.size do
+    count_actor+=1 if films[j][:actors].include? actors_arr[i]
+    j+=1
+  end
+  puts actors_arr[i] + " - " + count_actor.to_s 
+  i+=1
 end
 
-puts films.count
+
 
 
