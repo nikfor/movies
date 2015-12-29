b2c3c57296d76edb01b8119e1f240d0290c2d93e
nikfor <nikfor1@gmail.com>
2015-12-29 22:32:32 +0300

    ninth-task first attempt

--- my_movies_list.rb
+++ my_movies_list.rb
@@ -9,13 +9,34 @@ require 'csv'
 
 class MyMoviesList < MovieList
 
-  def initialize(path, separator)
-    @movie_arr = CSV.open(path.to_s, col_sep: separator.to_s).to_a.
+  def initialize(type = "txt", path = "movies.txt", separator = '|')
+    case type
+    when "txt" then
+      @movie_arr = CSV.open(path.to_s, col_sep: separator.to_s).to_a.
       map{ |row| Movie.create(row) }
+    when "html" then
+      @movie_arr = Array.new
+      doc = Nokogiri::HTML(open("http://www.imdb.com/chart/top"))
+      doc.search('td.titleColumn a').
+        each{ |a| 
+          film_page = Nokogiri::HTML(open("http://www.imdb.com#{a["href"]}"))
+          url = "http://www.imdb.com/chart/top#{a["href"]}"
+          name = a.content.to_s
+          year = film_page.at('h1 span[id="titleYear"]').content.delete('()')[0..3].to_s
+          country = film_page.at("h4[text()='Country:'] ~ a").content.to_s
+          date = film_page.at('meta[itemprop="datePublished"]')["content"].to_s
+          genre = film_page.search('span[itemprop="genre"]').map{ |genre| genre.content }.join(",").to_s
+          duration = film_page.at('time[itemprop="duration"]')["datetime"].delete("PTM").concat(" min").to_s
+          point = film_page.at('span[itemprop="ratingValue"]').content.to_s
+          author = film_page.at('span[itemprop="director"] a span').content.to_s
+          actors = film_page.search('span[itemprop="actors"] a span').map{ |act| act.content }.join(",").to_s
+          @movie_arr.push(Movie.create([url, name, year, country, date, genre, duration, point, author, actors]))
+        }
+    end
   end
 
   def info
-    @movie_arr.each{ |m| puts "#{m.name} #{m.genre}" }
+    @movie_arr.each{ |m| puts "#{m.name} #{m.year} #{m.point} #{m.actors.join(", ")}" }
   end
 
   def user_score(name_of_mov, date_of_viewing, user_point)
@@ -43,6 +64,14 @@ class MyMoviesList < MovieList
     @movie_arr.reject{ |mov| mov.user_point.nil? }.
       sort_by{ |mov| mov.user_point *= rand(100)*(DateTime.now - mov.watched).to_i*mov.weight }.
       first(count_films)                       
+  end
+
+  def save_to_yaml (path)
+    File.open( path, "w") { |f| f.write(@movie_arr.to_yaml) }
+  end
+
+  def load_from_yaml (path)
+    @movie_arr = YAML.load (File.open(path))
   end
 
 end
